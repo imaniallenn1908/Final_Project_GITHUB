9d5f39ce653ed5425f6576c9b673e8bf9c6b121e
Imani Allen <imaniallen@Imanis-MacBook-Air-3.local>
2026-04-29 23:08:49 -0400

    Removed absoulte filepath

--- code/render_report.R
+++ code/render_report.R
@@ -1,7 +1,9 @@
 here::i_am("code/render_report.R")
 
+dir.create("report", showWarnings = FALSE, recursive = TRUE)
+
 rmarkdown::render(
-  input = "Final-Project.Rmd",
-  output_file = file.path("report", "Final-Project.html"),
+  input = "/final/Final-Project.Rmd",
+  output_file = "/final/report/Final-Project.html",
   knit_root_dir = "/final"
 )
