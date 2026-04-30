a035305051656bb4a3d04bb68872c82562f3d186
Imani Allen <imaniallen@Imanis-MacBook-Air-3.local>
2026-04-30 00:58:31 -0400

    updated code

--- code/render_report.R
+++ code/render_report.R
@@ -2,8 +2,4 @@ here::i_am("code/render_report.R")
 
 dir.create("report", showWarnings = FALSE, recursive = TRUE)
 
-rmarkdown::render(
-  input = "/final/Final-Project.Rmd",
-  output_file = "/final/report/Final-Project.html",
-  knit_root_dir = "/final"
-)
+rmarkdown::render("/final/Final-Project.Rmd", knit_root_dir = "/final/report")
